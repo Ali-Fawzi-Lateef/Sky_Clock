96f55b44e28a2b1d1bdca7859c3dd0674bedb0c3
Ali Fawzi <83194640+kenpachi01011@users.noreply.github.com>
2021-10-17 18:49:52 +0300

    first commit

---
+++ Sky_Clock/src/MyFrame.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyFrame extends JFrame {
+
+    /*Calendar calendar;*/
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+
+    String time;
+    String day;
+    String date;
+
+    MyFrame() throws InterruptedException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // for close button
+        this.setTitle("Sky Clock");
+        this.setLayout(new FlowLayout());
+        this.getContentPane().setBackground(Color.BLACK);
+        this.setSize(360,240);
+        this.setResizable(false);
+
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Garamond",Font.PLAIN,60));
+        timeLabel.setForeground(new Color(0, 204, 255));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setOpaque(true);                                     // Display Background color
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Garamond",Font.PLAIN,50));
+        dayLabel.setForeground(new Color(0, 204, 255));
+        dayLabel.setBackground(Color.BLACK);
+        dayLabel.setOpaque(true);
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Garamond",Font.PLAIN,40));
+        dateLabel.setForeground(new Color(0, 204, 255));
+        dateLabel.setBackground(Color.BLACK);
+        dateLabel.setOpaque(true);
+
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTime();
+    }
+
+    public void setTime() throws InterruptedException {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+            Thread.sleep(1000);
+        }
+    }
+}
